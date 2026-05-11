f2073a318ed4b906a6439788c555bf74bb615d32
Ogawa <nicolasogawa1312@gmail.com>
2026-05-11 20:46:29 -0300

    Obtendo um usuário por id

--- src/main/java/com/nicolasogawa/workshopmongo/services/UserService.java
+++ src/main/java/com/nicolasogawa/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.nicolasogawa.workshopmongo.services;
 
 import com.nicolasogawa.workshopmongo.domain.User;
 import com.nicolasogawa.workshopmongo.repository.UserRepository;
+import com.nicolasogawa.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException ("Usuário não encontrado"));
     }
 }
