4106ada578185c58f59be976bff877c8b8140153
Ogawa <nicolasogawa1312@gmail.com>
2026-05-11 21:59:14 -0300

    Inserção de usuário com POST

--- src/main/java/com/nicolasogawa/workshopmongo/services/UserService.java
+++ src/main/java/com/nicolasogawa/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.nicolasogawa.workshopmongo.services;
 
 import com.nicolasogawa.workshopmongo.domain.User;
+import com.nicolasogawa.workshopmongo.dto.UserDTO;
 import com.nicolasogawa.workshopmongo.repository.UserRepository;
 import com.nicolasogawa.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,13 @@ public class UserService {
         Optional<User> obj = repo.findById(id);
 
         return obj.orElseThrow(() -> new ObjectNotFoundException ("Usuário não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User (objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
