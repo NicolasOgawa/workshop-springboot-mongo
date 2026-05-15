613d6310ac66e646b5a420a63fcb5175e0d88bc5
Ogawa <nicolasogawa1312@gmail.com>
2026-05-14 23:19:33 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/nicolasogawa/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nicolasogawa/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.nicolasogawa.workshopmongo.config;
 
 import com.nicolasogawa.workshopmongo.domain.Post;
 import com.nicolasogawa.workshopmongo.domain.User;
+import com.nicolasogawa.workshopmongo.dto.AuthorDTO;
 import com.nicolasogawa.workshopmongo.repository.PostRepository;
 import com.nicolasogawa.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,12 +33,13 @@ public class Instantiation implements CommandLineRunner {
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
-        User bob = new User(null, "Mingau", "mingau@gmail.com");
+        User mingau = new User(null, "Mingau", "mingau@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2026"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2026"), "Bom dia", "Acordei feliz hoje", maria);
+        userRepository.saveAll(Arrays.asList(maria, alex, mingau));
 
-        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        Post post1 = new Post(null, sdf.parse("21/03/2026"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2026"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(mingau));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
---
+++ src/main/java/com/nicolasogawa/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,39 @@
+package com.nicolasogawa.workshopmongo.dto;
+
+import com.nicolasogawa.workshopmongo.domain.User;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+
+    @Serial
+    public static final long serialVersionUID = 1l;
+
+    private String id;
+    private String name;
+
+    public AuthorDTO() {
+    }
+
+    public AuthorDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
