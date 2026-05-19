656ac8010abd9e608335db03c98fb57c33f72e70
Ogawa <nicolasogawa1312@gmail.com>
2026-05-18 22:56:02 -0300

    Endpointpara retornar os posts de um usuário

--- src/main/java/com/nicolasogawa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nicolasogawa/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.nicolasogawa.workshopmongo.resources;
 
+import com.nicolasogawa.workshopmongo.domain.Post;
 import com.nicolasogawa.workshopmongo.domain.User;
 import com.nicolasogawa.workshopmongo.dto.UserDTO;
 import com.nicolasogawa.workshopmongo.services.UserService;
@@ -54,5 +55,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
