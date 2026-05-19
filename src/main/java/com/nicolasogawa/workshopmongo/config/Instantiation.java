20375ce394184adffb3ef16cb0b6022ddfe098f4
Ogawa <nicolasogawa1312@gmail.com>
2026-05-19 01:10:25 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/nicolasogawa/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nicolasogawa/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.nicolasogawa.workshopmongo.config;
 import com.nicolasogawa.workshopmongo.domain.Post;
 import com.nicolasogawa.workshopmongo.domain.User;
 import com.nicolasogawa.workshopmongo.dto.AuthorDTO;
+import com.nicolasogawa.workshopmongo.dto.CommentDTO;
 import com.nicolasogawa.workshopmongo.repository.PostRepository;
 import com.nicolasogawa.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2026"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2026"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(mingau));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("22/03/2026"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2026"), new AuthorDTO(mingau));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2026"), new AuthorDTO(maria));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
